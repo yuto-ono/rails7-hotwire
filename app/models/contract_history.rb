743943ac441db4c9fda2243691c2645a46a7c51e
Yuto Ono <stellarwindmaster@gmail.com>
2024-05-15 16:18:21 +0900

    フォーム修正

--- app/models/contract_history.rb
+++ app/models/contract_history.rb
@@ -1,2 +1,6 @@
 class ContractHistory < ApplicationRecord
+  validates :company, presence: true
+  validates :ampere,
+    presence: true,
+    numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
